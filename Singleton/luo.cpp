5f9181e521ed0588dd735bb636364e4316cf5f46
leo <61103620+luobaotai@users.noreply.github.com>
2022-12-22 18:59:18 +0800

    Create luo.cpp

---
+++ Singleton/luo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Singleton {
+public:
+	~Singleton() {
+		std::cout << "destructor called!" << std::endl;
+	}
+	Singleton(const Singleton&) = delete;
+	Singleton& operator = (const Singleton&) = delete;
+	
+
+	static Singleton& get_instance() {
+		static Singleton instance;
+		return instance;
+	}
+
+private:
+	Singleton() {
+		std::cout << "constructor called!" << std::endl;
+	}
+};
+
+
+int main() {
+	Singleton& instance_1 = Singleton::get_instance();
+	Singleton& instance_2 = Singleton::get_instance();
+
+	std::cin.get();
+}
